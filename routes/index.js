609721beee525d29f18ef19ff7c1f8b655280c79
lingyics <lingyics@gmail.com>
2015-07-23 16:31:09 -0400

    routes without sessions

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lingyis TEST PAGE' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+router.post('/login', function(req, res, next) {
+  var user={
+    username:'admin',
+    password:'admin'
+  };
+  if(req.body.username===user.username && req.body.password===user.password){
+    res.redirect('/home');
+  }
+  res.redirect('/login');
+
+});
+
+router.get('/logout', function(req, res, next) {
+
+  res.redirect('/');
+});
+
+
+
+
+router.get('/home',function(req,res,next){
+  var user={
+    username:'admin',
+    password:'admin'
+  };
+  res.render('home',{title: 'Home Page', user:user});
+});
+
+module.exports = router;
