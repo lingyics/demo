ef805a0360560c032075c211ea657a9a03daaf4c
lingyics <lingyics@gmail.com>
2015-08-13 18:10:01 -0400

    modify gitignore

--- routes/starcat.js
+++ routes/starcat.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('This is the Page for Star Cat');
+    res.render('starcat',{title:'STAR CAT'});
 });
 
 router.get('/catlove', function(req, res, next) {
